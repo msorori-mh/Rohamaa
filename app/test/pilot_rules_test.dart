2c3077c7d27f91ccb2ba1c0bdb8395d73964e38e
Mokhtar Hussein Abdulwahab Alsorori <tarasana4const@gmail.com>
2026-09-06 18:50:09 +0300

    test: cover pilot domain rules

---
+++ app/test/pilot_rules_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sanad/src/domain/pilot_rules.dart';
+
+void main() {
+  group('PilotRules', () {
+    test('accepts only configured contribution amounts', () {
+      expect(PilotRules.isAllowedContribution(1000), isTrue);
+      expect(PilotRules.isAllowedContribution(5000), isTrue);
+      expect(PilotRules.isAllowedContribution(1500), isFalse);
+      expect(PilotRules.isAllowedContribution(0), isFalse);
+    });
+
+    test('handoff PIN must be exactly four digits', () {
+      expect(PilotRules.isValidHandoffPin('0000'), isTrue);
+      expect(PilotRules.isValidHandoffPin('4821'), isTrue);
+      expect(PilotRules.isValidHandoffPin('482'), isFalse);
+      expect(PilotRules.isValidHandoffPin('48a1'), isFalse);
+    });
+
+    test('donation image limit is four', () {
+      expect(PilotRules.canUploadMoreImages(0), isTrue);
+      expect(PilotRules.canUploadMoreImages(3), isTrue);
+      expect(PilotRules.canUploadMoreImages(4), isFalse);
+    });
+  });
+}
